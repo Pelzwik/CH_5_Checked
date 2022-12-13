f86ad9dd5c40e9b935e609656143b19e8805d202
Adam C <adam.clement@snhu.edu>
2022-12-13 11:37:30 -0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView  # new
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):  # new
+    model = Post
+    template_name = "post_detail.html"
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+
+application = get_wsgi_application()
